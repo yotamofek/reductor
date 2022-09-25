6ded3b8b67b913871c8bc8fc5acbc91dac0c0343
Yotam Ofek <yotam.ofek@gmail.com>
2022-09-25 08:15:46 +0000

    Typo: similary -> similarly.

--- src/reductors/count.rs
+++ src/reductors/count.rs
@@ -2,7 +2,7 @@ use std::{convert::TryInto, num::NonZeroUsize};
 
 use crate::Reductor;
 
-/// Reductor that counts the number of items yielded by an iterator (similary to [`Iterator::count`]).
+/// Reductor that counts the number of items yielded by an iterator (similarly to [`Iterator::count`]).
 #[repr(transparent)]
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Count(pub usize);
